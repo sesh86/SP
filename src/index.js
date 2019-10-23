ca17426584d4c9ccd02c441460464e3da41959cb
Ranjithcse <1997ranjithkumar@gmail.com>
2019-10-23 09:30:56 +0530

    Topicedit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Course from './components/Course';
 import Dashboard from './components/Dashboard';
 import Topic from './components/Topic';
 import TopicAdd from './components/TopicAdd';
+import Topicedit from './components/Topicedit';
 
 import {BrowserRouter,Route} from 'react-router-dom';
 
@@ -22,6 +23,7 @@ ReactDOM.render(
         <Route path="/dashboard" component={Dashboard}/>
         <Route path="/topics" component={Topic}/>
         <Route path="/topic-add" component={TopicAdd}/>
+        <Route path="/topic-edit" component={Topicedit}/>
     
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
