70add8242ffebedebb50db5216fdb2e6bc8a5c9d
Ranjithcse <1997ranjithkumar@gmail.com>
2019-10-23 09:41:09 +0530

    TopicEdit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Course from './components/Course';
 import Dashboard from './components/Dashboard';
 import Topic from './components/Topic';
 import TopicAdd from './components/TopicAdd';
-import Topicedit from './components/Topicedit';
+import TopicEdit from './components/TopicEdit';
 
 import {BrowserRouter,Route} from 'react-router-dom';
 
@@ -23,11 +23,9 @@ ReactDOM.render(
         <Route path="/dashboard" component={Dashboard}/>
         <Route path="/topics" component={Topic}/>
         <Route path="/topic-add" component={TopicAdd}/>
-        <Route path="/topic-edit" component={Topicedit}/>
+        <Route path="/topic-edit" component={TopicEdit}/>
     
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      
     </div>
     </BrowserRouter>
     , document.getElementById('root'));
